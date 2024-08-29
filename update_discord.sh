dcf2b28d13d19a5cbde080a9da2b535158545d76
Restart <45609733+RestartFU@users.noreply.github.com>
2024-08-28 20:28:29 -0400

    update

--- update_discord.sh
+++ update_discord.sh
@@ -1,8 +1,14 @@
-rm -rf ~/.discord
-
 download_url=$(curl -w "%{redirect_url}" -o /dev/null -s "https://discord.com/api/download?platform=linux&format=tar.gz")
 curl -o discord.tar.gz $download_url
 tar -xf discord.tar.gz
+
+if [ ! -d Discord/ ]; then
+    echo "Directory not found!"
+    exit 1
+fi
+
+rm -rf ~/.discord
+sudo rm -rf /usr/bin/discord
 mv Discord ~/.discord
 
 sudo ln -s $(realpath ~/.discord/Discord) /usr/bin/discord
